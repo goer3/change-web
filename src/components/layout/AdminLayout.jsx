2179af33c91961ddaba863dcbb2be3f12dfd5e32
KU4NG <1214966109@qq.com>
2023-10-22 21:44:12 +0800

    样式文件

--- src/components/layout/AdminLayout.jsx
+++ src/components/layout/AdminLayout.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '/src/assets/css/Admin.less';
 
 // 后台框架
 const AdminLayout = () => {
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ConfigProvider } from 'antd'; // 由于 antd 组件的默认文案是英文，所以需要修改为中文
+import zhCN from 'antd/es/locale/zh_CN';
+import 'antd/dist/antd.css'; // Antd 样式
+import 'misans/lib/misans-400-regular.min.css'; // 字体
+import '/src/assets/css/AntdOverwrite.css'; // 修改默认样式
+
+import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // autoInsertSpaceInButton：解决按钮的文本为两个汉字时中间自动补充空格的问题
+  <ConfigProvider locale={zhCN} autoInsertSpaceInButton={false}>
     <App />
-  </React.StrictMode>,
-)
+  </ConfigProvider>,
+);
