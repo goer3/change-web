7fe1ec0960cfad6fb504c13267652dcd83f096c8
KU4NG <1214966109@qq.com>
2023-11-10 08:16:19 +0800

    logo update

--- src/common/Resource.jsx
+++ src/common/Resource.jsx
@@ -1,4 +1,6 @@
-import Logo from '/src/assets/images/logo/logo.png';
+// import Logo from '/src/assets/images/logo/logo.png';
+// import Logo from '/src/assets/images/logo/logo-blue-white.png';
+import Logo from '/src/assets/images/logo/logo-black-white.png';
 import LogoWhite from '/src/assets/images/logo/logo-white.png';
 import DefaultAvatar from '/src/assets/images/avatar/avatar.jpg';
 
