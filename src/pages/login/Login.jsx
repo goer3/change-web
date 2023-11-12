2d22347a3dc85b2a274ff8f5fa2dc8630bcce994
KU4NG <1214966109@qq.com>
2023-11-13 00:22:05 +0800

    登录页面

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import '/src/assets/css/Login.less';
+import { DingtalkOutlined, LockOutlined, MailOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Checkbox, Divider, Form, Input, Space } from 'antd';
 import { Logo, LogoGray } from '../../common/Resource.jsx';
+import '/src/assets/css/Login.less';
 
 // 注释
 const Login = () => {
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+  };
   return (
     <>
       <div className="hero">
@@ -28,6 +33,67 @@ const Login = () => {
             </div>
             <div className="login-box-right">
               <div className="login-title">Sign in</div>
+              <Divider className="login-line">Welcome</Divider>
+              <div>
+                <Form
+                  name="login"
+                  className="login-form"
+                  initialValues={{
+                    remember: true,
+                  }}
+                  onFinish={onFinish}>
+                  <Form.Item
+                    name="account"
+                    rules={[
+                      {
+                        required: true,
+                        message: '请使用工号 / 手机号 / 邮箱登录!',
+                      },
+                    ]}>
+                    <Input className="login-input" prefix={<UserOutlined />} placeholder="工号 / 手机号 / 邮箱" />
+                  </Form.Item>
+                  <Form.Item
+                    name="password"
+                    rules={[
+                      {
+                        required: true,
+                        message: '请输入你的密码!',
+                      },
+                    ]}>
+                    <Input className="login-input" prefix={<LockOutlined />} type="password" placeholder="密码" />
+                  </Form.Item>
+                  <Form.Item>
+                    <Space direction="horizontal">
+                      <Input
+                        prefix={<MailOutlined />}
+                        placeholder="输入验证码"
+                        style={{
+                          width: '160px',
+                        }}
+                      />
+                      <Button className="login-code-btn">获取验证码</Button>
+                    </Space>
+                  </Form.Item>
+                  <Form.Item>
+                    <Form.Item name="remember" valuePropName="checked" noStyle>
+                      <Checkbox>记住密码</Checkbox>
+                    </Form.Item>
+                    <a href="">忘记密码？</a>
+                  </Form.Item>
+
+                  <Form.Item>
+                    <Button htmlType="submit" className="login-form-button">
+                      登录
+                    </Button>
+                    <div>
+                      没有账号？
+                      <a href="">
+                        直接钉钉扫码自动创建 <DingtalkOutlined />
+                      </a>
+                    </div>
+                  </Form.Item>
+                </Form>
+              </div>
             </div>
           </div>
         </div>
